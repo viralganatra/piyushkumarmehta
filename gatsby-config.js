6872193476e296951a770736c3e4f27906bd59c5
Viral Ganatra <viral@viralganatra.com>
2019-08-07 22:13:29 +0100

    Add title and meta description tags to index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,5 @@
  */
 
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/layout';
 import SectionIntro from '../components/home-page/intro';
 import SectionAbout from '../components/home-page/about';
@@ -6,6 +7,14 @@ import SectionAbout from '../components/home-page/about';
 export default function HomePage() {
   return (
     <Layout>
+      <Helmet>
+        <html lang="en-GB" />
+        <title>Hindu Wedding Priest - Silent Ceremonies | Piyushkumar Mehta</title>
+        <meta
+          name="description"
+          content="Hi. I am a Hindu wedding priest based in London who performs silent ceremonies, reciting our sacred Sanksrit verses whilst explaining in English."
+        />
+      </Helmet>
       <SectionIntro />
       <main role="main">
         <SectionAbout />
